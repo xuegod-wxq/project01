4ac5d70c8ec5ced2aca30b607119dd5f12a4b6fe
wxq <wxq@atguigu.com>
2022-04-29 15:45:36 +0800

    my second java commit

--- src/myfirstpackages/project01.java
+++ src/myfirstpackages/project01.java
@@ -3,5 +3,6 @@ package myfirstpackages;
 public class project01 {
     public static void main(String[] args){
         System.out.println("hello world!！");
+        System.out.println("hello java!");
     }
 }
